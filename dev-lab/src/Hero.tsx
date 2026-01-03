0c44302d0d05cbce9089ebd531d28f94a5e818da
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-03 22:06:01 +0100

    added custom color

--- dev-lab/src/Hero.tsx
+++ dev-lab/src/Hero.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='container container mx-auto px-4 py-6 md:py-12'>
-        hero
+    <div className='container  mx-auto px-4 py-6 md:py-12'>
+        <h1 className='text-primary-black'>hero</h1>
     </div>
   )
 }
