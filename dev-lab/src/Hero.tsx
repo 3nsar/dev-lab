14acdc112c76626e2736e19fe7ce7de3cae67494
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-03 22:33:59 +0100

    added hero section

--- dev-lab/src/Hero.tsx
+++ dev-lab/src/Hero.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='container  mx-auto px-4 py-6 md:py-12'>
-        <h1 className='text-primary-black'>hero</h1>
+    <div className='text-primary-white  bg-cyan-950 min-h-screen flex justify-center items-center flex-col '>
+            <div className='container mx-auto px-4 py-6 md:py-12 '>
+                <h1 className='text-8xl text-center'>Soo tasty....</h1>
+                <div className='flex justify-center items-center'>
+                    <button className='p-5 bg-primary-black '>Food</button>
+                    <button className='p-5 bg-primary-black '>Members</button>
+                </div>
+            </div>
     </div>
   )
 }
