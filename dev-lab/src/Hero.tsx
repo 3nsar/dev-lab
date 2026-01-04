17097ad5eb06b10c79588434ad1ac2a374357ac1
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-04 21:02:56 +0100

    button #1

--- dev-lab/src/Hero.tsx
+++ dev-lab/src/Hero.tsx
@@ -4,37 +4,26 @@ const Hero = () => {
   return (
     <div className="bg-cyan-950 min-h-screen flex justify-center items-center flex-col">
       <div className="container mx-auto px-4 py-6 md:py-12">
-        <h1 className="text-8xl text-center text-primary-white">Soo tasty....</h1>
+        <h1 className="text-8xl text-center font-extrabold text-primary-white">Soo tasty....</h1>
 
         <div className="flex justify-center items-center gap-6 mt-10">
-          <div className="relative group">
-            <button className="relative inline-block p-px font-semibold leading-6 text-primary-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl transition-transform duration-300 hover:scale-105 active:scale-95">
-              <span className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
 
-              <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
-                <div className="flex items-center space-x-2">
-                  <span className="transition-all duration-500 group-hover:translate-x-1">
-                    Let's get started
+
+              <div className="relative">
+                <button className="relative inline-block font-semibold leading-6 text-slate-100 cursor-pointer rounded-2xl p-px group">
+                  <span className="absolute inset-0 rounded-2xl bg-linear-to-r from-green-400 via-cyan-400 to-blue-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+                  <span className="absolute inset-0 rounded-2xl bg-slate-950 group-hover:opacity-0 transition-opacity duration-300" />
+
+                  <span className="relative block px-6 py-4 rounded-2xl bg-slate-950">
+                    <div className="flex items-center justify-center gap-x-1.5">
+                      <span className="font-semibold">
+                        SELECT YOUR UFO
+                      </span>
+                      <span className='text-xl'>👽</span>
+                    </div>
                   </span>
-
-                  <svg
-                    className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      clipRule="evenodd"
-                      fillRule="evenodd"
-                      d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                    />
-                  </svg>
-                </div>
-              </span>
-            </button>
-
-
-            
-          </div>
+                </button>
+              </div>
 
         </div>
       </div>
