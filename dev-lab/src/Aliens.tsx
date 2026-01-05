b31dd1bb5dfb17272e96ec0d16b169aac4e34eec
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-05 19:28:06 +0100

    commit

--- dev-lab/src/Aliens.tsx
+++ dev-lab/src/Aliens.tsx
@@ -19,13 +19,17 @@ const Aliens = () => {
 
   return (
     <div className="bg-linear-to-b from-black via-slate-050 to-purple-950 min-h-screen text-primary-white  flex justify-center">
-        <div className="container mx-auto px-4 py-6 md:py-12 grid grid-cols-3">
+        <div className="container mx-auto gap-10 md:gap-10 grid grid-cols-3">
+            <div className="flex items-center">
+            <input type="checkbox" name="checkbox" className="w-4 h-4 accent-purple-800 cursor-pointer" />
+            
+</div>
         {aliens.map((alien) => (
-            <div key={alien.name}>
-                <p className="font-bold">{alien.name}</p>
-                <p>Age: {alien.age}</p>
-                <p>Planet: {alien.planet}</p>
+            <div key={alien.name} className="rounded-2xl bg-slate-950 border-2 border-purple-800 flex justify-center items-center flex-col">
                 <img src={alien.img} alt={alien.name} />
+                <p className="font-bold text-2xl mt-5">{alien.name}</p>
+                <p className="text-lg">Age: {alien.age}</p>
+                <p className="text-lg">Planet: {alien.planet}</p>
             </div>
         ))}
         </div>
