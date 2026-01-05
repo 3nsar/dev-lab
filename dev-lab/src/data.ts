0b787c62046f90763b2e18e28232fc88a69c07e4
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-05 18:58:45 +0100

    json data

---
+++ dev-lab/src/data.ts
@@ -0,0 +1,42 @@
+export default[
+  
+  
+    {
+    "name": "Soro",
+    "age": "99",
+    "planet": "Mars",
+    "color": "gray",
+    "height": "2.2",
+    "img": "https://robohash.org/alien1?set=set2",
+    
+  },
+    {
+    "name": "Xerop10",
+    "age": "5",
+    "planet": "Venus",
+    "color": "black",
+    "height": "1.2",
+    "img": "https://robohash.org/alien2?set=set2",
+    
+  },
+      {
+    "name": "Soro",
+    "age": "99",
+    "planet": "Mars",
+    "color": "gray",
+    "height": "2.2",
+    "img": "https://robohash.org/alien1?set=set2",
+    
+  },
+    {
+    "name": "Xerop10",
+    "age": "5",
+    "planet": "Venus",
+    "color": "black",
+    "height": "1.2",
+    "img": "https://robohash.org/alien2?set=set2",
+    
+  },
+
+
+]
