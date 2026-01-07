98050a186c0084114e48289a8ecd78a081ee987c
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-07 22:13:00 +0100

    data update

--- dev-lab/src/data.ts
+++ dev-lab/src/data.ts
@@ -3,47 +3,47 @@ export default[
   
     {
     "name": "Soro",
-    "age": "99",
+    "age": 99,
     "planet": "Mars",
     "color": "orange",
-    "height": "2.2",
+    "height": 2.2,
     "img": "https://robohash.org/alien1?set=set2",
     
   },
     {
     "name": "Ajoja",
-    "age": "445",
+    "age": 445,
     "planet": "Kepler 442B",
     "color": "yellow",
-    "height": "5",
+    "height": 5,
     "img": "https://robohash.org/alien2?set=set2",
     
   },
       {
     "name": "Genoaloa",
-    "age": "39",
+    "age": 99,
     "planet": "Proxima Centaur",
     "color": "green",
-    "height": "2.2",
+    "height": 2.2,
     "img": "https://robohash.org/alien5?set=set2",
     
   },
     {
     "name": "Lotus",
-    "age": "53",
+    "age": 53,
     "planet": "Venus",
     "color": "pink",
-    "height": "1.2",
+    "height": 1.2,
     "img": "https://robohash.org/alien6?set=set2",
     
   },
 
       {
     "name": "Han",
-    "age": "38",
+    "age": 38,
     "planet": "mercury",
     "color": "green",
-    "height": "9",
+    "height": 9,
     "img": "https://robohash.org/alien7?set=set2",
     
   },
